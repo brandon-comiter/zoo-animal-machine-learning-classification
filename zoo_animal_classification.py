af8876ef93f8e7afb3b0b4999d7aaa71839158e0
Brandon Comiter <brandoncomiter@Brandons-MacBook-Pro-2691.local>
2019-09-03 16:07:29 -0400

    added headers to zoo dataframe

--- zoo_animal_classification.py
+++ zoo_animal_classification.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn import neighbors, preprocessing 
-from sklearn.models_selection import train_test_split
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib import style
 from math import sqrt
+from collections import Counter
 style.use('fivethirtyeight')
 
 
 
 def k_nearest_neighbors(dataset, predict):
     pass
+
+df = pd.read_csv('zoo.data', header= None)
+df.columns = ['Name', 'hair' , 'feathers', 'eggs', 'milk', 'aquatic',
+ 'predator', 'toothed', 'backbone', 'breathes', 'venomous', 'fins', 'legs',
+  'legs#', 'tail', 'domestic', 'catsize', 'classification']
+
+print(df)
+
