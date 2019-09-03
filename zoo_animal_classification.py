d925e142e678858f271b2d62458a2363653fb7ad
Brandon Comiter <brandoncomiter@Brandons-MacBook-Pro-2691.local>
2019-09-03 15:20:25 -0400

    added imports

---
+++ zoo_animal_classification.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn import neighbors, preprocessing 
+from sklearn.models_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib import style
+from math import sqrt
+style.use('fivethirtyeight')
+
+
+
+def k_nearest_neighbors(dataset, predict):
+    pass
